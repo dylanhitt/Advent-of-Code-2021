a5cafedc32862887a7173149f2cbb369ffccf81c
Hitt, Dylan F <dylan.hitt@gd-ms.com>
2021-12-07 10:28:34 -0500

    clean up day 3 code

--- day_3/main.go
+++ day_3/main.go
@@ -9,64 +9,34 @@ import (
 
 func main() {
 	lines, _ := common.ReadLines("input.txt")
-	half := len(lines) / 2
-	counts := map[int]int{}
-	for i := range lines[0] {
-		counts[i] = 0
-	}
-
-	for _, line := range lines {
-		for i, bit := range line {
-			if string(bit) == "1" {
-				counts[i]++
-			}
-		}
-	}
-
 	gamma := ""
 	epsilon := ""
-	for i := range lines[0] {
-		if counts[i] > half {
-			gamma = gamma + "1"
-			epsilon = epsilon + "0"
-			continue
-		}
-		gamma = gamma + "0"
-		epsilon = epsilon + "1"
+	for i := 0; i < len(lines[0]); i++ {
+		bit := commonBit(lines, i)
+		gamma = gamma + string(bit)
+		epsilon = epsilon + flipBit(bit)
 	}
 
 	gammaNu, _ := strconv.ParseInt(gamma, 2, 64)
 	epNum, _ := strconv.ParseInt(epsilon, 2, 64)
 	fmt.Println(gammaNu * epNum)
 
-	oxy := findOne(lines, 0, max)
-	co := findOne(lines, 0, min)
+	oxy, _ := strconv.ParseInt(findOne(lines, 0, max), 2, 64)
+	co, _ := strconv.ParseInt(findOne(lines, 0, min), 2, 64)
+	fmt.Println(oxy * co)
+}
 
-	oxyNum, _ := strconv.ParseInt(oxy, 2, 64)
-	fmt.Println(oxyNum)
-	coNum, _ := strconv.ParseInt(co, 2, 64)
-	fmt.Println(coNum)
-	fmt.Println(oxyNum * coNum)
+func commonBit(strings []string, position int) rune {
+	oneCount, zeroCount := getCount(strings, position)
+	return max(oneCount, zeroCount)
 }
 
 func findOne(strings []string, position int, f func(a int, b int) rune) string {
-	if len(strings) <= 1 {
+	if len(strings) == 1 {
 		return strings[0]
 	}
 
-	oneCount := 0
-	zeroCount := 0
-
-	for _, s := range strings {
-		bit := s[position]
-		if string(bit) == "1" {
-			oneCount++
-		}
-		if string(bit) == "0" {
-			zeroCount++
-		}
-	}
-
+	oneCount, zeroCount := getCount(strings, position)
 	filter := f(oneCount, zeroCount)
 	matched := []string{}
 	for _, s := range strings {
@@ -75,13 +45,27 @@ func findOne(strings []string, position int, f func(a int, b int) rune) string {
 		}
 	}
 
-	// if len(matched) == 0 {
-	// 	matched = strings
-	// }
-	fmt.Println(matched)
-
 	position++
 	return findOne(matched, position, f)
+}
+
+func getCount(strings []string, position int) (one int, zero int) {
+	for _, s := range strings {
+		bit := s[position]
+		if string(bit) == "1" {
+			one++
+			continue
+		}
+		zero++
+	}
+	return
+}
+
+func flipBit(bit rune) string {
+	if string(bit) == "1" {
+		return "0"
+	}
+	return "1"
 }
 
 func max(a, b int) rune {
